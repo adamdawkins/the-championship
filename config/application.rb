15a52782330383a621694a795ecbc2a7b4bb1611
Adam Dawkins <adam@dragondrop.uk>
2019-01-11 22:27:17 +0000

    🔧 Disable asset generators

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module TheChampionship
 
     config.generators do |g|
       g.test_framework :rspec
+      g.javascripts :false
+      g.stylesheets :false
+      g.assets :false
     end
   end
 end

ae644645d2abaf5456e849de292ef1dfacfa0f27
Adam Dawkins <adam@dragondrop.uk>
2019-01-12 00:09:31 +0000

    ✅ Add create contestant action

--- app/controllers/admin/contestants_controller.rb
+++ app/controllers/admin/contestants_controller.rb
@@ -2,4 +2,19 @@ class Admin::ContestantsController < AdminsController
   def new
     @contestant = Contestant.new
   end
+
+  def create
+    @contestant = Contestant.new(contestant_params)
+    if @contestant.save
+      redirect_to admin_contestants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def contestant_params
+      params.require(:contestant).permit(:name)
+    end
 end
--- spec/requests/admin/admin_contestants_spec.rb
+++ spec/requests/admin/admin_contestants_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Contestants", type: :request do
+  let(:leader) { create(:leader) }
+
   describe "GET /admin/contestants/new" do
     context "Leader not logged in" do
       before { get new_admin_contestant_path }
@@ -12,7 +14,6 @@ RSpec.describe "Admin::Contestants", type: :request do
     end
     
     context "Leader logged in" do
-      let(:leader) { create(:leader) }
       before do
         sign_in leader
         get new_admin_contestant_path
@@ -26,6 +27,33 @@ RSpec.describe "Admin::Contestants", type: :request do
          expect(response.body).to include("<form")
        end
     end
+  end
 
+  describe "POST /admin/contestants" do
+    let(:contestant_params) { { contestant: { name: Faker::Name.name } } }
+    context "Leader not logged in" do
+      before { post admin_contestants_path, { params: contestant_params } }
+
+      it "redirects to the login page" do
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to new_leader_session_path
+      end
+    end
+
+    context "Leader logged in" do
+      before do
+        sign_in leader
+      end
+
+      it "redirects successful posts to /admin/contestants" do
+        post admin_contestants_path, { params: contestant_params }
+        expect(response).to redirect_to("/admin/contestants")
+      end
+
+      it "renders the new template for unsuccessful posts" do
+        post admin_contestants_path, { params: { contestant:  { name: nil } } }
+        expect(response).to render_template("new")
+      end
+    end
   end
 end
